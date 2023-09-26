75be762dd2aa109dcb54e2e330b8cf53b4602737
Yuyuko9961 <mcfangkuaijun@outlook.com>
2023-09-26 15:23:33 +0800

    修正exp2中部分错误

--- Exp2/exp2_6.py
+++ Exp2/exp2_6.py
@@ -1,4 +1,4 @@
-dict1 = {}
-for i in range(128):
-    dict1[i] = chr(i)
+dict1 = {i: chr(i) for i in range(128)}
 print(dict1)
+# for key, value in dict1.items():
+#     print("%3d:%3s%s" % (key, value if key >= 32 else ' ', "|\n" if key % 10 == 9 else "|"), end="")
